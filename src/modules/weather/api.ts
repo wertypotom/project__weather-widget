175c40d9436122f502df00a58549ba356cb8cac5
Andrey Povstyanko <werty.potom@gmail.com>
2023-08-05 15:41:41 +0600

    changed request to https

--- src/modules/weather/api.ts
+++ src/modules/weather/api.ts
@@ -2,6 +2,6 @@ export const WEATHER_API_URL_BY_ID =
   'https://api.openweathermap.org/data/2.5/weather';
 
 export const WEATHER_API_URL_BY_CITY =
-  'http://api.openweathermap.org/geo/1.0/direct';
+  'https://api.openweathermap.org/geo/1.0/direct';
 
 export const WEATHER_API_KEY = 'c09e83e4a1a55a335ad660611073bbe0';
